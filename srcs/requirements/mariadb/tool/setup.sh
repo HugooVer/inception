4719a69910989a1c383fcd1dd2f036299b2e5a9b
HugooVer <hugo.vercelloneg@gmail.com>
2024-04-07 19:21:44 +0200

    work in progress

--- srcs/requirements/mariadb/tool/setup.sh
+++ srcs/requirements/mariadb/tool/setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+mkdir ~/data/database
+
 service mariadb start
 
-# variabels for tests
-
-mariadb -v -u root << EOF
+mariadb -v << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+SET PASSWORD FOR '$DB_ADMIN'@'localhost' = PASSWORD('$DB_PASS_ROOT');
 EOF
 
 sleep 5
 service mariadb stop
 
+exec $@
