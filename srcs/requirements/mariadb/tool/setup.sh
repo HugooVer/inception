2e6cf3fce1fcb3d48c822f431ccfeff1d4a9a068
HugooVer <hugo.vercelloneg@gmail.com>
2024-04-10 12:27:07 +0200

    all good

--- srcs/requirements/mariadb/tool/setup.sh
+++ srcs/requirements/mariadb/tool/setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-mkdir ~/data/database
-
 service mariadb start
 
-mariadb -v << EOF
+# db creation and users + give privilage
+mariadb -v -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
-SET PASSWORD FOR '$DB_ADMIN'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
 EOF
 
+# wait for good creation
 sleep 5
+
 service mariadb stop
 
+# launch the service with mysqld_safe
 exec $@
--- srcs/requirements/wordpress/tool/setup.sh
+++ srcs/requirements/wordpress/tool/setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-mkdir ~/data/wordpress_files
-
+# change the owner of the web site directory
 chown -R www-data:www-data /var/www/inception/
 
+# copy of the wp-config.php file
 if [ ! -f "/var/www/inception/wp-config.php" ]; then
    mv /tmp/wp-config.php /var/www/inception/
 fi
 
-sleep 10
+sleep 20
 
+# wordpress file download
 wp --allow-root --path="/var/www/inception/" core download || true
 
+# instalation of all downloaded file and admin user creation
 if ! wp --allow-root --path="/var/www/inception/" core is-installed;
 then
     wp  --allow-root --path="/var/www/inception/" core install \
@@ -20,8 +22,20 @@ then
         --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASSWORD \
         --admin_email=$WP_ADMIN_EMAIL
+
+    # delet all useless plugins
+    wp --allow-root --path="/var/www/inception/" plugin delete hello
+    wp --allow-root --path="/var/www/inception/" plugin delete akismet
 fi;
 
+# check af good instalation of wordpress
+if ! wp --allow-root --path="/var/www/inception/" core is-installed;
+then
+    echo "Failed to install wordpress"
+    exit 1
+fi;
+
+# non admin user creation
 if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
 then
     wp  --allow-root --path="/var/www/inception/" user create \
@@ -31,6 +45,9 @@ then
         --role=$WP_ROLE
 fi;
 
-wp --allow-root --path="/var/www/inception/" theme install raft --activate 
+# change oner of sone directory
+chown -R www-data:www-data /var/www/inception/
+chmod -R 755 /var/www/*
 
+# start of php-fpm in forgrownd
 exec $@
